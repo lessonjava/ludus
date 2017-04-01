b5f714f0c54da80010feb5fe0a9df13b5c6a5052
siiji <siiji@192.168.1.5>
2017-04-02 05:17:16 +0900

    sql書き換え

--- sql/userInsert.sql
+++ sql/userInsert.sql
@@ -1,48 +1,47 @@
-
 DELETE FROM users;
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("aaa@gmail.com","12345678","赤羽根","あかばね","1234567","東京都","北区","08011112222",3);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","赤羽根","あかばね","1234567","東京都","北区","08011112222","aaa@gmail.com",3);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("bbb@gmail.com","12345678","秋月律子","あきづきりつこ","1234567","東京都","　　","08011112222",3);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","秋月","あきづき","1234567","東京都","　　","08011112222","bbb@gmail.com",3);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("ccc@gmail.com","12345678","音無小鳥","おとなしことり","1234567","東京都","　　","08011112222",2);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","音無","おとなし","1234567","東京都","　　","08011112222","ccc@gmail.com",2);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("haruka@gmail.com","12345678","天海春香","あまみはるか","1234567","神奈川県","二宮町","08011112222",1);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","天海","あまみ","1234567","神奈川県","二宮町","08011112222","haruka@gmail.com",1);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("chihaya@gmail.com","12345678","如月千早","きさらぎちはや","1234567","東京都","渋谷区","08011112222",1);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","如月","きさら","1234567","東京都","渋谷区","08011112222","chihaya@gmail.com",1);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("miki@gmail.com","12345678","星井美希","ほしいみき","1234567","東京都","港区","08011112222",1);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","星井","ほしい","1234567","東京都","港区","08011112222","miki@gmail.com",1);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("yayoi@gmail.com","12345678","高槻やよい","たかつきやよい","1234567","東京都","調布市","08011112222",1);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","高槻","たかつき","1234567","東京都","調布市","08011112222","yayoi@gmail.com",1);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("yukiho@gmail.com","12345678","萩原雪歩","はぎわらゆきほ","1234567","東京都","足立区","08011112222",1);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","萩原","はぎわら","1234567","東京都","足立区","08011112222","yukiho@gmail.com",1);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("makoto@gmail.com","12345678","菊池真","きくちまこと","1234567","東京都","　　","08011112222",1);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","菊池","きくち","1234567","東京都","　　","08011112222","makoto@gmail.com",1);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("mami@gmail.com","12345678","双海真美","ふたみまみ","1234567","東京都","　　","08011112222",1);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","双海","ふたみ","1234567","東京都","　　","08011112222","mami@gmail.com",1);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("ami@gmail.com","12345678","双海亜美","ふたみあみ","1234567","東京都","　　","08011112222",1);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","双海","ふたみ","1234567","東京都","　　","08011112222","ami@gmail.com",1);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("iori@gmail.com","12345678","水瀬伊織","みなせいおり","1234567","東京都","　　","08011112222",1);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","水瀬","みなせ","1234567","東京都","　　","08011112222","iori@gmail.com",1);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("azusa@gmail.com","12345678","三浦あずさ","みうらあずさ","1234567","東京都","　　","08011112222",1);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","三浦","みうら","1234567","東京都","　　","08011112222","azusa@gmail.com",1);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("hibiki@gmail.com","12345678","我那覇響","がなはひびき","1234567","東京都","　　","08011112222",1);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","我那覇","がなは","1234567","東京都","　　","08011112222","hibiki@gmail.com",1);
 
-insert into users(mail_adrress,password,name_kanji,name_hiragana,postal,address1,address2,phone_number,user_authority)
-values("takane@gmail.com","12345678","四条貴音","しじょうたかね","1234567","東京都","　　","08011112222",1);
+insert into users(password,name_kanji,name_hira,postal,address1,address2,phone_number,phone_email,user_flg)
+values("1234","四条","しじょう","1234567","東京都","　　","08011112222","takane@gmail.com",1);
 
